d98421a91d4f86945ff0402d5c74569aa5a1cfdd
spamer7777-lgtm <spamer7777@gmail.com>
2025-11-10 13:16:32 +0100

    Update GeneralCommands.cs

--- commands/GeneralCommands.cs
+++ commands/GeneralCommands.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Interactions;
@@ -21,6 +23,33 @@ namespace Commands
         {
             await RespondAsync(text: $"👋 HEEEJ! {user.Mention}!");
         }
-    }
 
+        // 🎰 New Slots command
+        [CommandContextType(InteractionContextType.Guild, InteractionContextType.BotDm, InteractionContextType.PrivateChannel)]
+        [IntegrationType(ApplicationIntegrationType.UserInstall, ApplicationIntegrationType.GuildInstall)]
+        [SlashCommand("slots", "Try your luck at 777 Slots!")]
+        public async Task Slots()
+        {
+            string[] icons = { "🍒", "🍋", "🍉", "💎", "7️⃣" };
+            var rand = new Random();
+
+            // Generate 3 random slot symbols
+            var result = Enumerable.Range(0, 3)
+                .Select(_ => icons[rand.Next(icons.Length)])
+                .ToArray();
+
+            string output = string.Join(" ", result);
+            bool win = result.Distinct().Count() == 1;
+
+            var embed = new EmbedBuilder()
+                .WithTitle("🎰 777 Slots 🎰")
+                .WithDescription($"**{output}**\n" +
+                                 (win ? "💰 **JACKPOT! WYGRAŁEŚ/AŚ!**" : "😢 Następnym razem odda..."))
+                .WithColor(win ? Color.Gold : Color.DarkGrey)
+                .WithFooter($"Played by {Context.User.Username}", Context.User.GetAvatarUrl())
+                .Build();
+
+            await RespondAsync(embed: embed);
+        }
+    }
 }
