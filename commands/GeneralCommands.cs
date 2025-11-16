b2125c4920a35c9aa0d59757efe91bbdedcdb021
spamer7777-lgtm <spamer7777@gmail.com>
2025-11-16 14:37:34 +0100

    Update GeneralCommands.cs

--- commands/GeneralCommands.cs
+++ commands/GeneralCommands.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using Discord;
 using Discord.Interactions;
 using Discord.WebSocket;
+using System.IO;
 
 namespace Commands
 {
@@ -282,6 +283,7 @@ namespace Commands
         }
     }
 }
+
 
 
 
