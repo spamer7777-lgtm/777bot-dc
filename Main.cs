b8e20a2726e21dd7aaddb899be3529396af7087c
spamer7777-lgtm <spamer7777@gmail.com>
2025-11-17 01:04:02 +0100

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -56,6 +56,9 @@ public static class Bot
         Client.Ready += Ready;
         Client.Log += Log;
         Client.MessageReceived += MessageReceivedHandler;
+
+        var cts = new CancellationTokenSource();
+        _ = HttpApi.StartAsync(cts.Token);
         
         await Client.LoginAsync(TokenType.Bot, Token);
         await Client.StartAsync();
@@ -259,6 +262,7 @@ public static class Bot
         return Task.CompletedTask;
     }
 }
+
 
 
 
