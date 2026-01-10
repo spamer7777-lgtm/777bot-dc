144073454ceb38dca7a42b2ca68a79a4f5647c20
spamer7777-lgtm <spamer7777@gmail.com>
2026-01-10 22:03:44 +0100

    Create PendingWycenaState.cs

---
+++ PendingWycenaState.cs
@@ -0,0 +1,19 @@
+namespace _777bot;
+
+public enum PendingKind
+{
+    WaitingForVehiclePaste,
+    WaitingForSpecialColorPrices
+}
+
+public class PendingWycenaState
+{
+    public PendingKind Kind { get; set; }
+    public int Vuid { get; set; }
+    public ulong UserId { get; set; }
+    public ulong ChannelId { get; set; }
+    public DateTime ExpiresAtUtc { get; set; }
+
+    // when asking for special color prices:
+    public List<(SpecialColorType type, string name, string rarity)> MissingSpecialColors { get; set; } = new();
+}
